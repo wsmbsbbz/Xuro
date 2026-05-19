32674d8d9540ca539ffc74f1f38fd12f495056d6
wsmbsbbz <wsmbsbbz@gmail.com>
2026-05-19 21:59:33 +0800

    Remove local VS Code debug config

--- .vscode/env.sh
+++
@@ -1,4 +0,0 @@
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
-export ANDROID_HOME=/opt/android-sdk
-export ANDROID_SDK_ROOT=/opt/android-sdk
-export PATH="$JAVA_HOME/bin:$ANDROID_HOME/emulator:$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/tools/bin:$PATH"
